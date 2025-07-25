d29c51f4efe23eace7da74946bcb4306ce839654
catcatcatok <670977359@qq.com>
2025-07-25 15:12:39 +0800

    feat: 集成 Rocksi 并支持 iframe 加载

--- Astral3DEditorGoBack/main.go
+++ Astral3DEditorGoBack/main.go
@@ -25,27 +25,22 @@ func init() {
 		panic(err2.Error())
 	}
 
-	// 注册各模块
-	utils.InitLogger() //调用logger初始化
+	utils.InitLogger()
 }
 
 func main() {
-	dist := "C:/Users/67097/Desktop/新建文件夹/Rocksi-master/dist/build" // 改成你的绝对路径
+	// 使用相对路径，让别人也能运行
+	dist := filepath.Join("static", "rocksi")
 
-	// 主目录
 	beego.BConfig.WebConfig.StaticDir["/rocksi"] = dist
-	// 额外把代码里用到的绝对路径也映射一下
 	beego.BConfig.WebConfig.StaticDir["/models"] = filepath.Join(dist, "models")
 	beego.BConfig.WebConfig.StaticDir["/i18n"] = filepath.Join(dist, "i18n")
 	beego.BConfig.WebConfig.StaticDir["/images"] = filepath.Join(dist, "images")
 
-	// 老接口也可以：beego.SetStaticPath("/rocksi", rocksiAbsPath)
-	// ====== 以上新增 ======
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
-	//var appPath, _ = beego.AppConfig.String("currentAbPath")
-	//fmt.Println("当前项目路径:", appPath)
+
 	beego.Run()
 }
